2a041a48175048f98e6b4923743d1bcd86a2bb33
ertu96 <48797125+ertu96@users.noreply.github.com>
2023-07-15 22:57:26 +0200

    proxy workaround

--- src/api/projects/getProjects.ts
+++ src/api/projects/getProjects.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export const getProjects = async () => {
 	try {
-		const response = await axios.get(`/github/users/${import.meta.env.VITE_GITHUB_USERNAME}/repos`);
+		const response = await axios.get(
+			`https://api.github.com/users/${import.meta.env.VITE_GITHUB_USERNAME}/repos`
+		);
 		return response.data;
 	} catch (error) {
 		console.error(error);
