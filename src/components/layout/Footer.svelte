bce303aba6f8febbf3185cdef084f21dc5878648
ertu96 <48797125+ertu96@users.noreply.github.com>
2024-05-26 09:16:17 +0200

    footer current year update

--- src/components/layout/Footer.svelte
+++ src/components/layout/Footer.svelte
@@ -1,7 +1,16 @@
+<script>
+	/**
+	 * @type {number}
+	 */
+	const currentYear = new Date().getFullYear();
+
+</script>
+
+
 <footer
 	class="footer items-center py-3 bottom-0 text-gray-900 dark:text-gray-50 w-full bg-gray-50 dark:bg-gray-900 z-20"
 >
 	<div class="container px-4 flex flex-wrap justify-between items-center mx-auto">
-		<p class="text-xs font-thin">Copyright © 2023 - Ertugrul Kurnaz</p>
+		<p class="text-xs font-thin">Copyright © {currentYear} - Ertugrul Kurnaz</p>
 	</div>
 </footer>
