76ecdd6f04e0b485d22f63222da84ef8385af5fd
ertu96 <48797125+ertu96@users.noreply.github.com>
2024-05-30 09:30:21 +0200

    dependency updates

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,9 +1,10 @@
+// eslint.config.js
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
+	ignorePatterns: ['*.cjs', '.eslintrc.js'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
