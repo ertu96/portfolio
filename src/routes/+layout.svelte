d8ede438ea28a5fb5f2299603b8ac5e743aa25e5
ertu96 <48797125+ertu96@users.noreply.github.com>
2024-06-21 21:18:53 +0200

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.css';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { inject } from '@vercel/analytics';
+	import { dev } from '$app/environment';
+
+	injectSpeedInsights();
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
